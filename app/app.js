abc20906c5ac89511836f48cb5a22d4f2158738e
titustesche <titustesche@outlook.de>
2024-02-08 18:07:44 +0100

    Commented the code

--- app/app.js
+++ app/app.js
@@ -1,16 +1,19 @@
+// Modules needed for functionality
 const Rcon = require('rcon-client').Rcon;
 const { Server } = require('socket.io');
 
+
+// Login credentials for the rcon server
 const host = '127.0.0.1';
 const port = 25575;
 const password = '1234';
 
-// Funktion zum senden von Befehlen
+// Function to send commands to rcon server
 async function sendCommand(cmd, callback)
 {
-    // Initialisiere rcon Verbindung
+    // initialize rcon connection
     const rcon = new Rcon({host, port, password});
-    // Versuche mit dem rcon Server zu verbinden und den Befehl zu senden
+    // Try to connect to the rcon server, send the command and callback the server's reply
     try
     {
         await rcon.connect();
@@ -18,31 +21,49 @@ async function sendCommand(cmd, callback)
         await rcon.end();
         // Gebe die Server Antwort zurück
         console.log(response  + " in function");
-        callback(response);
+        callback("" + response);
     }
 
-    // Falls das fehlschlägt, gebe den Fehler zurück
+    // If that fails, reply with the given error message
     catch (error)
     {
-        callback(error);
+        callback("" + error);
     }
 }
 
+// Create new websocket using Socket.io
 const io = new Server({
     cors: {
         origin: "*",
     }
 });
 
+// Event receiver for handling communication between user and server
 io.on("connection", (socket) => {
     console.log("Client connected");
+    // Send an empty test command to the server to test if it is reachable
+    sendCommand('', (function (reply) {
+        if (reply.indexOf('Error') > -1)
+        {
+            // Throw an error when the connection fails
+            socket.emit('reply', '[Backend] Error while trying to connect to host: ' + reply);
+        }
+
+        else
+        {
+            // Notify the user when a connection can be established
+            socket.emit('reply', '[Backend] Successfully connected to rcon server!');
+        }
+    }));
+    // Handling the request event
     socket.on('request', (cmd) => {
+        // When the request Event is triggered, send the command over to the rcon Server, wait for it to respond and pass the reply back to the frontend
         sendCommand(cmd, function (reply) {
             socket.emit('reply', reply);
         });
     })
 })
 
-io.listen(3000, () => {
-    console.log('listening on port 3000');
-});
+
+// Listen for incoming connections
+io.listen(3000);
--- frontend/includes/main.inc.js
+++ frontend/includes/main.inc.js
@@ -1,19 +1,22 @@
+// Try to connect to the Node websocket
 const socket = io('http://127.0.0.1:3000');
 
+// If the connection was established, notify the user
 socket.on("connect", () => {
     responseOutput.textContent = responseOutput.textContent + "Successfully connected to backend! \n";
 })
 
+// Get control elements from the page
 const sendBtn = document.getElementById('sendButton');
 const commandInput = document.getElementById('commandInput');
 const responseOutput = document.getElementById('responseOutput');
 
+// Output any messages received from the reply event to the user
 socket.on('reply', (reply) => {
-    //console.log(reply);
     responseOutput.textContent = responseOutput.textContent + reply + "\n";
 })
 
+// Emit the request event with the contents of the input field
 sendBtn.addEventListener('click', () => {
     socket.emit('request', commandInput.value);
-    //responseOutput.textContent = responseOutput.textContent + "Sent: " + commandInput.value + "\n";
 })
